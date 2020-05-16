2658694fc099fd920e3d35990fa4ab24a7731a15
mitevdimitar <mitev.dimitar@gmail.com>
2020-05-16 22:07:05 +0300

    html, body and root widht and height set to 100%

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 
 import "./styles/index.css";
 
-import App from "./App";
 import Navigation from "./Navigation";
 
 ReactDOM.render(<Navigation />, document.getElementById("root"));

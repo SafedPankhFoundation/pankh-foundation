1699e15a838e78fe5c0c15fe8a64118063ca7a85
mitevdimitar <mitev.dimitar@gmail.com>
2020-05-19 14:34:26 +0300

    handle email and password input fields functionality added

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import SigninForm from "./SigninForm";
 import SignupField from "./SignupField";
 
 const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
+const handleEmailChange = (e) => {
+    console.log(e.target.value)
+    setEmail(e.target.value);
+}
+
+const handlePasswordChange = (e) => {
+    console.log(e.target.value)
+    setPassword(e.target.value);
+}
+
+const handleUserLogin = (e) => {
+    e.preventDefault();
+    console.log(email);
+    console.log(password)
+    // handle user athentification
+}
+
+const loginInfo = {
+  email,
+  handleEmailChange,
+  password,
+  handlePasswordChange,
+  handleUserLogin
+}
+
   return (
     <div className="sign-in">
-      <SigninForm />
+      <SigninForm loginInfo={loginInfo}/>
       <SignupField />
     </div>
   );
--- src/components/SignIn/SigninForm.jsx
+++ src/components/SignIn/SigninForm.jsx
@@ -1,24 +1,25 @@
 import React from "react";
 
-const SigninForm = () => {
+const SigninForm = (props) => {
+  const {email, handleEmailChange, password, handlePasswordChange, handleUserLogin} = props.loginInfo;
   return (
       <div className="signin-form">
         <div className="left-container">
           <form action="/#" method="post">
             <h1>Sign In to your Account</h1>
             <h3>Login to your account with UserId & Password</h3>
-            <input type="text" name="email" placeholder="Email" /><br></br>
-            <input type="password" name="password" placeholder="Password" /><br></br>
+            <input type="text" name="email" placeholder="Email" value={email} onChange={handleEmailChange}/><br></br>
+            <input type="password" name="password" placeholder="Password" value={password} onChange={handlePasswordChange}/><br></br>
             <div className="password-options">
               <div>
                 <input type="checkbox" id="remember-me" name="remember" value="remember" />
-                <label for="remember-me">Remember Me</label>
+                <label htmlFor="remember-me">Remember Me</label>
               </div>
               <div>
                 <span>Forgot password</span>
               </div>
             </div>
-            <input className="signin-button" type="submit" value="Sign In"/>
+            <input className="signin-button" type="submit" value="Sign In" onClick={handleUserLogin}/>
             <p>Social icons</p>
           </form>
         </div>

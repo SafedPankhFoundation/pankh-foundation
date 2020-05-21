a4288f8da87f546ba8190acff1d2582945d323e6
mitevdimitar <mitev.dimitar@gmail.com>
2020-05-21 13:06:28 +0300

    sign in page layout optymized

--- src/components/SignIn/SignupField.jsx
+++ src/components/SignIn/SignupField.jsx
@@ -3,12 +3,15 @@ import React from "react";
 const SignupField = () => {
   return (
       <div className="signup-field">
-        {/* this div is needed to push the content float right */}
-        <div className="left-fill"></div>
-        <div className="right-container">
+        <div className="right-green-container">
+          {/* this div is needed to push the content float right */}
+          <div className="left-fill">
+          </div>
+          <div className="right-signup-proposal">
           <h1>Don't have account yet?</h1>
           <p>Lets get you all set that can create on your first on boarding experience</p>
           <input className="signup-button" type="submit" value="Sign Up"/>
+          </div>
         </div>
       </div>
   );
